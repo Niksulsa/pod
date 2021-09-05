ba3a68416ea85d219d35174dc8902f75f3d0f0eb
Nikhitha Ulsah <87293732+Niksulsa@users.noreply.github.com>
2021-09-05 18:13:06 -0400

    Cards styling

--- src/components/Picture.js
+++ src/components/Picture.js
@@ -1,11 +1,14 @@
 import React from "react";
+import './picture.scss';
 
 const Picture = props => {
     return(
-        <div>
-      <h3>{props.photo.title}</h3>
-      <img src={props.photo.url} alt={props.photo.title} />
-      <p>{props.photo.explanation}</p>
+      <div className="card">
+        <div className="card__main">
+            <h3 className="card__heading">{props.photo.title}</h3>
+            <img className="card__image" src={props.photo.url} alt={props.photo.title} />
+        </div>
+        <p className="card__description">{props.photo.explanation}</p>
     </div>
     )
 }
